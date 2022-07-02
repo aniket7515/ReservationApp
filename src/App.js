4d057c826bad06054e0000048c739bb2a0bc368d
Aniket <aniketdhokane2071@gmail.com>
2022-07-02 20:13:21 +0530

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './pages/home/Home'
+import List from './pages/list/List';
+import Hotel from './pages/hotel/Hotel';
 
 function App() {
   return (
-    <div className="App">
-      Helllo
-    </div>
+    <Router>
+       <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/hotels' element={<List/>}/>
+          <Route path='/hotels/:id' element={<Hotel/>}/>
+
+
+
+       </Routes>
+    
+    
+    </Router>
   );
 }
 
