8a3ec3bd9debe25c68d81d3dba6375b82c05775c
Aniket <aniketdhokane2071@gmail.com>
2022-07-02 22:00:00 +0530

    Header design

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import './header.css'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faBed,faPlane,faCar,faTaxi} from "@fortawesome/free-solid-svg-icons"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header">
+        <div className="headerContainer">
+        <div className="headerList">
+            <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+            </div>
+            <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+            </div>
+            <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+            </div>
+            <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+            </div>
+            <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport Taxis</span>
+            </div>
+
+        </div>
+        <h1 className="headerTitle">A lifetime of discounts? It's Genius</h1>
+        <p className="headerDesc">Get Rewarded for your travels - unlock instant saving of 10% or more with a free Lamabooking account</p>
+        <button className="headerBtn">Sign in/Register</button>
+        </div>
+        
+    </div>
   )
 }
 
