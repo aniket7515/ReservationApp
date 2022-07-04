7ec355f29c49e29de59219423a6833623afbc924
Aniket <aniketdhokane2071@gmail.com>
2022-07-04 22:24:43 +0530

    React footer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Featured from '../../components/featured/Featured'
 import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
+import Footer from '../../components/footer/Footer'
 import Header from '../../components/header/Header'
 import MailList from '../../components/mailList/MailList'
 import Navbar from '../../components/navbar/Navbar'
@@ -23,6 +24,8 @@ const Home = () => {
          <FeaturedProperties/>
          <MailList/>
       </div>
+
+      <Footer/>
     </div>
   )
 }
